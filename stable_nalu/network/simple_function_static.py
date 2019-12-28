eec7abe977929be2f24f4bb29a1fcf4a97e0675f
hzhexuan <48533350+hzhexuan@users.noreply.github.com>
2019-12-28 12:01:37 +0100

    Update simple_function_static.py

--- stable_nalu/network/simple_function_static.py
+++ stable_nalu/network/simple_function_static.py
@@ -143,81 +143,46 @@ class ReversedFunctionStaticNetwork(ExtendedTorchModule):
 class MultiFunctionStaticNetwork(ExtendedTorchModule):
     UNIT_NAMES = GeneralizedLayer.UNIT_NAMES
 
-    def __init__(self, unit_name, input_size=100, hidden_size_add=8, hidden_size_mul = 32, writer=None, first_layer=None, nac_mul='none', eps=1e-7, **kwags):
+    def __init__(self, unit_name, input_size=100, hidden_size = [8, 32], writer=None, first_layer=None, nac_mul='none', eps=1e-7, **kwags):
         super().__init__('network', writer=writer, **kwags)
         self.unit_name = unit_name
         self.input_size = input_size
         self.nac_mul = nac_mul
         self.eps = eps
-
-        if first_layer is not None:
-            unit_name_1 = first_layer
-        else:
-            unit_name_1 = unit_name
-
-        self.layer_1 = GeneralizedLayer(input_size, hidden_size_add,
-                                        unit_name_1,
+        hidden_size.insert(0, input_size)
+        l = len(hidden_size)
+        hidden_size.append(1)
+        
+        self.layers = []
+        for i in range(l):
+            if(i % 2 == 0):
+                self.layers.append(GeneralizedLayer(hidden_size[i], hidden_size[i+1],
+                                        unit_name,
                                         writer=self.writer,
-                                        name='layer_1',
-                                        eps=eps, **kwags)
-
-        if nac_mul == 'mnac':
-            unit_name_2 = unit_name[0:-3] + 'MNAC'
-        else:
-            unit_name_2 = unit_name
-
-        self.layer_2 = GeneralizedLayer(hidden_size_add, hidden_size_mul,
-                                        'linear' if unit_name_2 in BasicLayer.ACTIVATIONS else unit_name_2,
+                                        name='layer'+str(i+1),
+                                        eps=eps, **kwags))
+            else:
+                self.layers.append(GeneralizedLayer(hidden_size[i], hidden_size[i+1],
+                                        unit_name[0:-3] + 'MNAC',
                                         writer=self.writer,
-                                        name='layer_2',
-                                        eps=eps, **kwags)
-        unit_name_3 = unit_name
-
-        self.layer_3 = GeneralizedLayer(hidden_size_mul, 1,
-                                        unit_name_3,
-                                        writer=self.writer,
-                                        name='layer_3',
-                                        eps=eps, **kwags)
-        print(unit_name_1, unit_name_2, unit_name_3)
-        print(input_size,hidden_size_add)
+                                        name='layer'+str(i+1),
+                                        eps=eps, **kwags))
+        
         self.reset_parameters()
-        self.z_1_stored = None
 
     def reset_parameters(self):
-        self.layer_1.reset_parameters()
-        self.layer_2.reset_parameters()
-        self.layer_3.reset_parameters()
+        for layer in self.layers:
+            layer.reset_parameters()
 
     def regualizer(self):
-        if self.nac_mul == 'max-safe':
-            return super().regualizer({
-                'z': torch.mean(torch.relu(1 - self.z_1_stored))
-            })
-        else:
-            return super().regualizer()
+        return super().regualizer()
 
     def forward(self, input):
         self.writer.add_summary('x', input)
-        z_1 = self.layer_1(input)
-        self.z_1_stored = z_1
-        self.writer.add_summary('z_1', z_1)
-
-        if self.nac_mul == 'none' or self.nac_mul == 'mnac':
-            z_2 = self.layer_2(z_1)
-        elif self.nac_mul == 'normal':
-            z_2 = torch.exp(self.layer_2(torch.log(torch.abs(z_1) + self.eps)))
-        elif self.nac_mul == 'safe':
-            z_2 = torch.exp(self.layer_2(torch.log(torch.abs(z_1 - 1) + 1)))
-        elif self.nac_mul == 'max-safe':
-            z_2 = torch.exp(self.layer_2(torch.log(torch.relu(z_1 - 1) + 1)))
-        else:
-            raise ValueError(f'Unsupported nac_mul option ({self.nac_mul})')
-
-        self.writer.add_summary('z_2', z_2)
-        
-        z_3 = self.layer_3(z_2)
-        self.writer.add_summary('z_3', z_3)
-        return z_3
+        for i in range(l):
+            input = self.layers[i](input)
+            self.writer.add_summary('z_' + str(i+1), input)
+        return input
 
     def extra_repr(self):
         return 'unit_name={}, input_size={}'.format(
