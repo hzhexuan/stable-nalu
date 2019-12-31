c499716cc46bc0d482f2c9cfe932a2d43108b039
hzhexuan <48533350+hzhexuan@users.noreply.github.com>
2019-12-31 05:10:33 +0100

    Update myexp_sparse_batch.py

--- experiments/myexp_sparse_batch.py
+++ experiments/myexp_sparse_batch.py
@@ -203,7 +203,7 @@ for e in args.hidden_size:
     hidden_size += " "+str(e)
 out = os.popen("python experiments/myexp_sparse.py \
     --batch-size "+str(args.batch_size)+" --learning-rate "+str(args.learning_rate)+" --regualizer "+str(args.regualizer)+" \
-    --regualizer-scaling-start "+str(args.regulizer_scaling_start)+" --regualizer-scaling-end "+str(args.regulizer_scaling_end)+" \
+    --regualizer-scaling-start "+str(args.regualizer_scaling_start)+" --regualizer-scaling-end "+str(args.regualizer_scaling_end)+" \
     --input-size 4 --interpolation-range [-1,1] --extrapolation-range [-1,1] \
     --size "+str(args.size)+" --hidden-size" + hidden_size + " --momentum "+str(args.momentum --percent)+" "+str(args.percent)+" \
     --operation mul --layer-type ReRegualizedLinearNAC --nac-mul mnac \
