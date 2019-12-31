b9ad1e93058569ede6e09b7f57d26285adf08c35
hzhexuan <48533350+hzhexuan@users.noreply.github.com>
2019-12-31 09:33:23 +0100

    Update myexp_sparse.py

--- experiments/myexp_sparse.py
+++ experiments/myexp_sparse.py
@@ -372,6 +372,7 @@ print(f'  - loss_train: {loss_train}')
 print(f'  - loss_valid_inter: {loss_valid_inter}')
 print(f'  - loss_valid_extra: {loss_valid_extra}')
 
+print(sum(L_in)/len(L_in), sum(L_ex)/len(L_ex), loss_valid_inter.cpu().numpy(), loss_valid_extra.cpu().numpy())
 # Use saved weights to visualize the intermediate values.
 #stable_nalu.writer.save_model(summary_writer.name, model)
 import sys
