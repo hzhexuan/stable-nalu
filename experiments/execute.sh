1d702cfb403350aa4e98ba6641380cc6465b1a24
hzhexuan <48533350+hzhexuan@users.noreply.github.com>
2019-12-31 09:59:18 +0100

    Update execute.sh

--- experiments/execute.sh
+++ experiments/execute.sh
@@ -1,9 +1,9 @@
 
 for i in {1..$1};do
-python experiments/myexp_sparse.py --batch-size 64 --learning-rate 0.01 --regualizer 20 --regualizer-scaling-start 10000 --regualizer-scaling-end 20000 \
-    --input-size 4 --interpolation-range [-1,1] --extrapolation-range [-1,1] \
-    --size $2 --hidden-size 16 24  --momentum 0.0 --percent $3\
-    --operation mul --layer-type ReRegualizedLinearNAC --nac-mul mnac \
-    --seed $i --max-iterations 20000 \
-    --name-prefix test --remove-existing-data \
+    python experiments/myexp_sparse.py --batch-size 64 --learning-rate 0.01 --regualizer 20 --regualizer-scaling-start 10000 --regualizer-scaling-end 20000 \
+        --input-size 4 --interpolation-range [-1,1] --extrapolation-range [-1,1] \
+        --size $2 --hidden-size 16 24  --momentum 0.0 --percent $3\
+        --operation mul --layer-type ReRegualizedLinearNAC --nac-mul mnac \
+        --seed $i --max-iterations 20000 \
+        --name-prefix test --remove-existing-data
 done
