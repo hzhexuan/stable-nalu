c27974451fbe02a1428ae43d5981e257bb12faa3
hzhexuan <48533350+hzhexuan@users.noreply.github.com>
2019-12-31 10:20:25 +0100

    Update execute.sh

--- experiments/execute.sh
+++ experiments/execute.sh
@@ -7,7 +7,8 @@ for i in $(seq 1 1 $1);do
         --operation mul --layer-type ReRegualizedLinearNAC --nac-mul mnac \
         --seed $i --max-iterations 20000 \
         --name-prefix test --remove-existing-data &
-    else
+    fi
+    if [$i -eq $1] then
     python experiments/myexp_sparse.py --batch-size 64 --learning-rate 0.01 --regualizer 20 --regualizer-scaling-start 10000 --regualizer-scaling-end 20000 \
         --input-size 4 --interpolation-range [-1,1] --extrapolation-range [-1,1] \
         --size $2 --hidden-size 16 24  --momentum 0.0 --percent $3\
