44b9117278e1524054df74d398427bade6b90b16
hzhexuan <48533350+hzhexuan@users.noreply.github.com>
2019-12-31 09:52:39 +0100

    Update execute.sh

--- experiments/execute.sh
+++ experiments/execute.sh
@@ -1,6 +1,6 @@
 
 for i in {1..$1};do
-python experiments/myexp_sparse.py --batch-size 64 --learning-rate 0.01 --regualizer 20 --regualizer-scaling-start 10000 --regualizer-scaling-end 20000 \
+chmod u+x python experiments/myexp_sparse.py --batch-size 64 --learning-rate 0.01 --regualizer 20 --regualizer-scaling-start 10000 --regualizer-scaling-end 20000 \
     --input-size 4 --interpolation-range [-1,1] --extrapolation-range [-1,1] \
     --size $2 --hidden-size 16 24  --momentum 0.0 --percent $3\
     --operation mul --layer-type ReRegualizedLinearNAC --nac-mul mnac \
