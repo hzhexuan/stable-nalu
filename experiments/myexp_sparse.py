b466b615a4ba3ddf7b1ba9b89a6c97d1afbe03f3
hzhexuan <48533350+hzhexuan@users.noreply.github.com>
2020-01-02 06:15:35 +0100

    Update myexp_sparse.py

--- experiments/myexp_sparse.py
+++ experiments/myexp_sparse.py
@@ -363,7 +363,8 @@ for epoch_i in range(args.max_iterations + 1):
     if(epoch_i%200 == 0):
       x, t= dataset_test_extrapolation_data
       print(model(x)[0], t[0])
-      print(x[0])
+      if(epoch_i == 0):
+        print(x[0].reshape([3,3]))
 # Compute validation loss
 loss_valid_inter = test_model(dataset_valid_interpolation_data)
 loss_valid_extra = test_model(dataset_test_extrapolation_data)
