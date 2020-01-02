d2ed3e1f8dc114b83e26c3c5fdc1263e2a3b8cb8
hzhexuan <48533350+hzhexuan@users.noreply.github.com>
2020-01-02 12:02:21 +0100

    Update temp.py

--- experiments/temp.py
+++ experiments/temp.py
@@ -264,7 +264,7 @@ if 'LSB_DJOB_NUMPROC' in os.environ:
 
 # setup model
 model = stable_nalu.network.ConvStaticNetwork(
-    1, 9, 3,
+    1, 24, 3,
     writer=summary_writer.every(1000).verbose(args.verbose),
     nac_oob=args.oob_mode,
     regualizer_shape=args.regualizer_shape,
