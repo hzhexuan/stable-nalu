622d23fa6cc21fa1f4f58a786703708ed823a347
hzhexuan <48533350+hzhexuan@users.noreply.github.com>
2020-01-02 10:39:18 +0100

    Update temp.py

--- experiments/temp.py
+++ experiments/temp.py
@@ -358,7 +358,7 @@ for epoch_i in range(args.max_iterations + 1):
     # Log gradients if in verbose mode
     if args.verbose and epoch_i % 1000 == 0:
         model.log_gradients()
-    if(epoch_i%200 == 0):
+    if(epoch_i%1000 == 0):
       x, t= dataset_test_extrapolation_data
       print(model(x)[0], t[0])
 # Compute validation loss
