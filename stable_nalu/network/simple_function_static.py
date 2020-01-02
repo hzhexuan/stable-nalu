84da5ba1c9cff873b7fb6f93f0b54a0451378d70
hzhexuan <48533350+hzhexuan@users.noreply.github.com>
2020-01-02 03:58:03 +0100

    Update simple_function_static.py

--- stable_nalu/network/simple_function_static.py
+++ stable_nalu/network/simple_function_static.py
@@ -193,7 +193,6 @@ class MultiFunctionStaticNetwork(ExtendedTorchModule):
 class ConvStaticNetwork(ExtendedTorchModule):
     def __init__(self, input_c, output_c, kernel, hidden_size=2, writer=None, first_layer=None, nac_mul='none', eps=1e-7, **kwags):
         print(kwags)
-        print(**kwags)
         self.kernel=kernel
         self.unfold_input = kernel * kernel * input_c
         self.unfold_output = output_c
