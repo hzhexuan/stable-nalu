dd12c23f58f6d16d6436bbd75fa7a3385a936c23
hzhexuan <48533350+hzhexuan@users.noreply.github.com>
2020-01-03 08:22:26 +0100

    Update simple_function_static.py

--- stable_nalu/network/simple_function_static.py
+++ stable_nalu/network/simple_function_static.py
@@ -200,7 +200,7 @@ class ConvStaticNetwork(ExtendedTorchModule):
         self.kernel=kernel
         self.unfold_input = kernel * kernel * input_c
         self.unfold_output = output_c
-        self.mask = torch.Tensor(scipy.linalg.block_diag(*[np.ones((hidden_size, 1)) for i in range(output_c)]))
+        self.mask = torch.Tensor(scipy.linalg.block_diag(*[np.ones((hidden_size, 1)) for i in range(output_c)]).T)
         #self.k = SimpleFunctionStaticNetwork('ReRegualizedLinearNAC', input_size=25, hidden_size=16, output_size=128, **kwags)
         #self.k2 = SimpleFunctionStaticNetwork('ReRegualizedLinearNAC', input_size=self.unfold_input, hidden_size=hidden_size, output_size=self.unfold_output, **kwags)
         #for i in range(n):
@@ -226,7 +226,6 @@ class ConvStaticNetwork(ExtendedTorchModule):
         B, S, W = list(windows.size())
         windows = windows.transpose(1, 2)
         for e in self.k.layer_2.parameters():
-            print(list(e.size()), list(self.mask.size()))
             e = e * self.mask
         processed = self.k(windows.reshape([-1, S])).reshape([B, W, -1]).transpose(1, 2)
         output_size = input_size - self.kernel + 1
